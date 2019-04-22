982bd354c3975c390372c6a41a77c303c28ec047
brobeson <brobeson@users.noreply.github.com>
2019-04-22 09:43:55 -0600

    try using catch2 for unit tests

--- unit_tests/point_test.cpp
+++ unit_tests/point_test.cpp
@@ -1,7 +1,16 @@
-#define BOOST_TEST_MODULE Ess Point Test
-#include <boost/test/unit_test.hpp>
+//#define BOOST_TEST_MODULE Ess Point Test
+//#include <boost/test/unit_test.hpp>
+//
+//BOOST_AUTO_TEST_CASE(Foo)  // NOLINT
+//{
+//  BOOST_CHECK_EQUAL(0, 0);
+//}
 
-BOOST_AUTO_TEST_CASE(Foo)  // NOLINT
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("Dummy test")
 {
-  BOOST_CHECK_EQUAL(0, 0);
+  REQUIRE(1 == 0);
 }
